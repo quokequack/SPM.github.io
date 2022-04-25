6d7c45c1a264ebac7e0a9dcd62dcc0fdd237d1fe
Malu Alaquoke <88796071+quokequack@users.noreply.github.com>
2022-04-24 21:14:04 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,16 +1,42 @@
-function confirmaSenha(){
-    var input1 = document.querySelector("#senha");
-    var senha1 = input1.value;
+const myform = document.querySelector("#myform");
+const user = document.querySelector("#user");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
+const death = document.querySelector("#death");
+const btn = document.querySelector("#btn");
+const errorMsg = document.querySelector(".error-msg");
 
-    var input2 = document.querySelector("#senha2");
-    var senha2 = input2.value;
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-    if(input1.value != input2.value){
-        document.getElementById("#senha2").style.color("red");
-        alert("As senhas não conferem!!!!!");
+    const userValue = user.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const password2Value = password2.value;
+
+    if(userValue === '' || emailValue === '' || passwordValue === '' || password2Value === ''){
+        errorMsg.textContent = "Preencha os campos!";
+        errorMsg.classList = "error";
+    }else{
+        classList = "inputSuccess";
     }
-    else{
-        document.getElementById("#senha2").style.color("green");
-        document.getElementById("#senha").style.color("green");
+    if(!isEmail(emailValue)){
+        errorMsg.textContent = "Formato de email inválido";
+        errorMsg.classList = "error";
     }
-}
+    if(passwordValue.length < 8 && passwordValue.length > 8){
+        errorMsg.textContent = "Senha deve ser maior do que 8 caracteres";
+        errorMsg.classList = "error";
+    }
+    if(passwordValue != password2Value){
+        errorMsg.textContent = "As senhas não conferem";
+        errorMsg.classList = "error";
+    }
+
+
+
+    function isEmail(email) {
+        return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+    }
+})
